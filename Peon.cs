117f486b2500469a43201394c560613be793bafe
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-10-01 22:24:33 -0300

    Se abstrae la logica del movimiento.

--- Peon.cs
+++ Peon.cs
@@ -7,10 +7,7 @@
         public override List<string> Movimientos()
         {
             var movimientos = new List<string>();
-            var x = Conversor.Coordenada(Posicion).x - 1;
-            var y = Conversor.Coordenada(Posicion).y + 1; // !!
-            if (x < 8 && x >= 0 && y < 8 && y >= 0)
-                movimientos.Add(Conversor.Codigo(x, y));
+            LogicaMovimientos.Norte1(movimientos, this);
             return movimientos;
         }
     }
